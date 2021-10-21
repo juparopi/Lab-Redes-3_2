3e3e69de0b5dde3d3d0630e380da37f6e6f76490
juparopi <jp.romero@uniandes.edu.co>
2021-10-20 20:18:25 -0500

    Final del codigo

--- Lab3_2Redes/Cliente/Cliente.py
+++ Lab3_2Redes/Cliente/Cliente.py
@@ -1,14 +1,19 @@
 import socket
 import time
+import hashlib
 import struct as st
 from os import path
 import threading
 
 numClientes = int(input('Ingrese el numero de clientes que desea crear>> '))
 
+
 def clientFunction(name):
-    PORT = 9006
+    
+    UDPPORT = name +9500
+    PORT = 9000
     FORMAT = 'utf-8'
+    #SERVER = "192.168.231.128"
     SERVER = "127.0.0.1"
     ADDR = (SERVER, PORT)
     FILES_DIR = 'archivos'
@@ -18,65 +23,91 @@ def clientFunction(name):
     abs_path = path.dirname(path.abspath(__file__))
     files_path = path.join(abs_path,FILES_DIR)
     logs_path = path.join(abs_path,LOGS_DIR)
-
+    
+    def obtener_hash(file_path):
+        hash_md5 = hashlib.md5()
+        with open(file_path, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_md5.update(chunk)
+        f.close()
+        return hash_md5.digest()
+    
     # Se crea el socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s = socket.socket()
+    s.connect(ADDR)
+    
+    #Se crea el socket udp
+    sUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
     # Se crea el log
     file_name = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())+'-log'+str(name)+'.txt'
     log = open(path.join(logs_path, file_name),'w')
     
+    # Recibe si el server esta listo
+    console_msg = str(s.recv(255).decode(FORMAT))
+
+    
     # Saluda al cliente
-    s.sendto("Ready".encode(FORMAT),ADDR)
+    s.send("Ready".encode(FORMAT))
     
     # Se registra el nombre del archivo
-    data1 , addr = s.recvfrom(255)
-    file_name = str(data1.decode(FORMAT))
+    file_name = str(s.recv(255).decode(FORMAT))
     log.write('Se recibe el archivo {} \n'.format(file_name))
     
     
     file_name = "Cliente"+str(name)+"-Prueba-"+str(numClientes)+".txt"
     file_path = path.join(files_path,file_name)
     
+    #Inicia la conexion udp
+    sUDP.sendto(b'Hello', (SERVER, UDPPORT))
+    
     # Se extrae el archivo y se determina su peso
     with open(file_path, 'wb') as f:
         # Se recibe el tamaño del archivo
-        data2, addr = s.recv(4)
-        file_size = int(st.unpack('I',data2)[0])
+        file_size = int(st.unpack('I',s.recv(4))[0])
         log.write('El archivo pesa {} bytes\n'.format(file_size))
-    
+        
         # Se guarda el archivo en el cliente    
         total_file_size = 0
-        while total_file_size < file_size:
-            data3, addr = s.recvfrom(10*1024)
-            total_file_size+=len(data3)
-            f.write(data3)
+        flag = True;
+        while flag:
+            data , addrudp = sUDP.recvfrom(1024)
+            total_file_size+=len(data)
+            tcpdata = s.recv(1024)
+            if tcpdata == b'termino':
+                flag = False
+            f.write(data)
         f.close()
-    file_size_local = path.getsize(file_path) 
+    sUDP.close()
+    
+    s.send('Archivo recibido'.encode(FORMAT))
+    # Se recibe el hash del server
+    checksum = s.recv(64)
+    checksum_local = obtener_hash(file_path)
     
     # Se determina si el archivo llego correctamente o no
-    if file_size_local == file_size:
-        s.sendto("ok".encode(FORMAT), ADDR)
+    if checksum == checksum_local:
+        s.send("ok".encode(FORMAT))
         console_msg = 'El archivo de '+str(file_size)+" bytes se recibio de forma exitosa"
         log.write(console_msg+'\n')
     else:
-        s.sendto("not ok".encode(FORMAT),ADDR)
+        s.send("not ok".encode(FORMAT))
         console_msg = 'El archivo no se recibio bien'
         log.write(console_msg+'\n')
         
     
     # Se envia el tiempo de terminacion
-    s.sendto(st.pack('d',time.time()), ADDR)
-    
+    s.send(st.pack('d',time.time()))
     
     # Se recibe el tiempo total
-    data4 , addr = s.recvfrom(8)
-    total_time = float(st.unpack('d',data4)[0])
+    total_time = float(st.unpack('d',s.recv(8))[0])
     log.write('Tiempo de transmicion fue: {} s \n'.format(round(total_time,3) ))
     log.write(console_msg+'\n')
     
     # Se cierra todo
     s.close()
     console_msg = 'Se cierra la conexion'
+    print(console_msg , name)
     log.close()
     
     
--- Lab3_2Redes/Server/Server.py
+++ Lab3_2Redes/Server/Server.py
@@ -1,10 +1,11 @@
 import socket
 import threading
-from os import path
+from os import path, listdir
 import time
+import hashlib
 import struct as st
 
-PORT = 9001
+PORT = 9000
 SERVER = "127.0.0.1"
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
@@ -15,9 +16,16 @@ all_conns = []
 abs_path = path.dirname(path.abspath(__file__))
 files_path = path.join(abs_path,FILES_DIR)
 logs_path = path.join(abs_path,LOGS_DIR)
-global server
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
+
+def obtener_hash(file_path):
+    hash_md5 = hashlib.md5()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    f.close()
+    return hash_md5.digest()
 
 
 # Elige que archivo se va a enviar y a cuantos clientes se va a esperar
@@ -29,6 +37,7 @@ def iniciar_server():
         global hash_local
         global num_usuarios
         
+        
         del all_conns[:]
         global num_usuarios
         num_usuarios = int(input('Numero de usuarios a enviar>> '))
@@ -37,6 +46,7 @@ def iniciar_server():
         file_name = files[int(input('Ingrese el archivo que desea usar (0 para 100Mb) (1 para 250Mb)>> '))]
         file_path = path.join(files_path,file_name)
         file_size = path.getsize(file_path)
+        hash_local = obtener_hash(file_path)
     
     except Exception as e :
         print('Ocurrio un error con las opciones elegidas',str(e))
@@ -45,15 +55,15 @@ def iniciar_server():
 # Atiende las diferentes peticiones
 def correr_server():
     serverNum = 0
+    server.listen(25)
     print(f"Server escuchando en {SERVER}")
     # Acepta todas las conexiones e inicia un thread para enviar cuando todos esten listos
     while True:
         try:
-            data1, addr = server.recvfrom(4096)
+            conn, addr = server.accept()
             server.setblocking(1)
-            client = ClientThread(addr,serverNum)
+            client = ClientThread(conn,addr,serverNum)
             serverNum = serverNum + 1
-            
             global all_conns
             all_conns.append(client)
             client.start()
@@ -77,74 +87,93 @@ def correr_server():
 
 # Thread que da manejo a cada cliente
 class ClientThread(threading.Thread):
-    
-    def __init__(self, addr,serverNum):
+    def __init__(self, conn, addr,serverNum):
         threading.Thread.__init__(self)
+        self.conn = conn
         self.addr = addr
         self.ready = False
         self.terminar = True
         self.send = False
         self.serverNum =  serverNum
+        self.udpport = 9500 + serverNum
     
     def run(self):
-        #Se crea el log para escribir los resultados
-        name = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())+'-log'+str(self.serverNum)+'.txt'
-        log = open(path.join(logs_path, name),'w')
+        #Se crea conexion UDP
+        sUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sUDP.bind((SERVER, self.udpport))
+        # Saluda al cliente
+        self.conn.send("Connected with server".encode(FORMAT))
+        # Recibe que el cliente esta listo
+        msg = self.conn.recv(255).decode(FORMAT)
         
-        self.ready = True
-        console_msg = 'La conexion {} esta lista \n'.format(self.addr)
-        print (console_msg)
-        log.write('Se le envia al cliente {} \n'.format(self.addr))
+        if msg == "Ready":
+            #Se crea el log para escribir los resultados
+            name = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())+'-log'+str(self.serverNum)+'.txt'
+            log = open(path.join(logs_path, name),'w')
+            
+            self.ready = True
+            console_msg = 'La conexion {} esta lista \n'.format(self.addr)
+            print (console_msg)
+            log.write('Se le envia al cliente {} \n'.format(self.addr))
 
-        
-        #Espera a poder enviar
-        while self.terminar and not self.send:
-            continue
-        if self.terminar and self.send:
-            server.sendto(file_name.encode(FORMAT), self.addr)
-            log.write('Se envia el archivo {} \n'.format(file_name))
             
-            # Se abre el archivo
-            f = open(file_path, 'rb')
-            
-            #Se encapsula el tamano y se envia 
-            file_size_encoded = st.pack('I', file_size)
-            server.sendto(file_size_encoded, self.addr)
-            log.write('El archivo pesa {} \n'.format(file_size))
-            line = f.read(1024)
-            start_time = time.time()
-            
-            # Se itera sobre el archivo para enviarse por paquetes
-            while line:
-                server.sendto(line, self.addr)
-                line = f.read(1024)
-            
-            
-            # Se lee si el cliente confirma que fue correcto o no
-            data2 , addr = server.recvfrom(255)
-            data2 = data2.decode(FORMAT)
-            if data2 == "ok":
-                log.write('El archivo se envio satisfactoriamente \n')
-            else:
-                log.write('El archivo no se envio satisfactoriamente\n')
-            
-            # Se toma y envia el tiempo al cliente
-            data3 , addr = server.recvfrom(8).decode(FORMAT)
-            stop_time = float(st.unpack('d',data3)[0])
-            total_time = abs(start_time - stop_time)
-            while data2 and data3:
+            #Espera a poder enviar
+            while self.terminar and not self.send:
                 continue
-            server.sendto(st.pack('d',total_time), self.addr)
-            
-            
-            # Se registra el tiempo y se cierra
-            log.write('Tiempo de transmicion fue: {} s \n'.format(round(total_time,3) ))
-            log.close()
-            self.conn.close()
-        else:
-            log.close()
-            self.conn.close()
+            if self.terminar and self.send:
+                self.conn.send(file_name.encode(FORMAT))
+                log.write('Se envia el archivo {} \n'.format(file_name))
+                log.write('El archivo pesa {} \n'.format(file_size))
                 
+                # Se abre el archivo
+                f = open(file_path, 'rb')
+                
+                #Se encapsula el tamano y se envia 
+                file_size_encoded = st.pack('I', file_size)
+                self.conn.send(file_size_encoded)
+                line = f.read(1024)
+                
+                
+                data1 , udpaddr = sUDP.recvfrom(4096)
+                start_time = time.time()
+                
+                # Se itera sobre el archivo para enviarse por paquetes
+                
+                while line:
+                    sent = sUDP.sendto(line, udpaddr)
+                    line = f.read(1024)
+                    self.conn.send(b' ')
+                self.conn.send(b'termino')
+                sUDP.close()
+                
+                
+                estado_transferencia = self.conn.recv(255).decode(FORMAT)
+                console_msg = 'La conexion {} dice {} \n'.format(self.addr,estado_transferencia)
+                print (console_msg)
+                
+                # Se le envia nuestro hash para que el cliente verifique
+                self.conn.send(hash_local)
+                
+                # Se lee si el cliente confirma que fue correcto o no
+                envio_correcto = self.conn.recv(255).decode(FORMAT)
+                if envio_correcto == "ok":
+                    log.write('El archivo se envio satisfactoriamente \n')
+                else:
+                    log.write('El archivo no se envio satisfactoriamente\n')
+                
+                # Se toma y envia el tiempo al cliente
+                stop_time = float(st.unpack('d',self.conn.recv(8))[0])
+                total_time = abs(start_time - stop_time)
+                self.conn.send(st.pack('d',total_time))
+                
+                # Se registra el tiempo y se cierra
+                log.write('Tiempo de transmicion fue: {} s \n'.format(round(total_time,3) ))
+                log.close()
+                self.conn.close()
+            else:
+                log.close()
+                self.conn.close()
+                    
                 
 iniciar_server()
 correr_server()
